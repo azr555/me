40d8755f6485afb705a8c4cf04e81c635422181d
azr555secret <60191913+azr555secret@users.noreply.github.com>
2020-03-13 03:24:52 +0700

    Update jav.php

--- jav.php
+++ jav.php
@@ -15,7 +15,7 @@ echo color("green"," =================================== \n");
 	$nama = nama();
 	$email = str_replace(" ", "", $nama) . mt_rand(100, 999);
 	ulang:
-	echo color("nevy","(?) Number : ");
+	echo color("nevy","?] Number : ");
 	$nohp = trim(fgets(STDIN));
 	$nohp = str_replace("62","62",$nohp);
 	$nohp = str_replace("(","",$nohp);
@@ -109,12 +109,17 @@ echo color("green"," =================================== \n");
 							
 							$cekvoucher = request('/gopoints/v3/wallet/vouchers?limit=10&page=1', $token);
 							$total = fetch_value($cekvoucher,'"total_vouchers":',',');
-							$voucher3 = getStr1('"title":"','",',$cekvoucher,"3");
 							$voucher1 = getStr1('"title":"','",',$cekvoucher,"1");
 							$voucher2 = getStr1('"title":"','",',$cekvoucher,"2");
+							$voucher3 = getStr1('"title":"','",',$cekvoucher,"3");
 							$voucher4 = getStr1('"title":"','",',$cekvoucher,"4");
 							$voucher5 = getStr1('"title":"','",',$cekvoucher,"5");
 							$voucher6 = getStr1('"title":"','",',$cekvoucher,"6");
+							$voucher7 = getStr1('"title":"','",',$cekvoucher,"7");
+							$voucher8 = getStr1('"title":"','",',$cekvoucher,"8");
+							$voucher9 = getStr1('"title":"','",',$cekvoucher,"9");
+							$voucher10 = getStr1('"title":"','",',$cekvoucher,"10");
+							
 							echo "\n".color("yellow","!] Total voucher ".$total." : ");
 							echo "\n".color("green","1. ".$voucher1);
 							echo "\n".color("green","2. ".$voucher2);
@@ -122,12 +127,22 @@ echo color("green"," =================================== \n");
 							echo "\n".color("green","4. ".$voucher4);
 							echo "\n".color("green","5. ".$voucher5);
 							echo "\n".color("green","6. ".$voucher6);
+							echo "\n".color("green","7. ".$voucher7);
+							echo "\n".color("green","8. ".$voucher8);
+							echo "\n".color("green","9. ".$voucher9);
+							echo "\n".color("green","10. ".$voucher10);
+							
 							$expired1 = getStr1('"expiry_date":"','"',$cekvoucher,'1');
 							$expired2 = getStr1('"expiry_date":"','"',$cekvoucher,'2');
 							$expired3 = getStr1('"expiry_date":"','"',$cekvoucher,'3');
 							$expired4 = getStr1('"expiry_date":"','"',$cekvoucher,'4');
 							$expired5 = getStr1('"expiry_date":"','"',$cekvoucher,'5');
 							$expired6 = getStr1('"expiry_date":"','"',$cekvoucher,'6');
+							$expired7 = getStr1('"expiry_date":"','"',$cekvoucher,'7');
+							$expired8 = getStr1('"expiry_date":"','"',$cekvoucher,'8');
+							$expired9 = getStr1('"expiry_date":"','"',$cekvoucher,'9');
+							$expired10 = getStr1('"expiry_date":"','"',$cekvoucher,'10');
+							
 							setpin:
 							echo "\n".color("nevy","?] Set PIN? (Y/N) : ");
 							$pilih1 = trim(fgets(STDIN));
@@ -152,13 +167,13 @@ echo color("green"," =================================== \n");
 				goto setpin;
 			}
 		} else {
-			echo color("red","x] The code you entered is incorrect");
+			echo color("red","x] The code you entered is incorrect!!! \n");
 			echo color("yellow","!] Please input again \n");
 			goto otp;
 		}
 	} else {
 		echo color("red","x] This number already registered!!!");
-		echo "\nMau ulang? (y/n): ";
+		echo "\n Try Again? (Y/N): ";
 		$pilih = trim(fgets(STDIN));
 		if($pilih == "y" || $pilih == "Y") {
 			echo color("yellow","!] Please register again using other number \n");
