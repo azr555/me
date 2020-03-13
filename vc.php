379a927115ac6f0ec192a9318e98432425ab9635
azr555secret <60191913+azr555secret@users.noreply.github.com>
2020-03-13 20:33:22 +0700

    Update vc.php

--- vc.php
+++ vc.php
@@ -2,13 +2,13 @@
 date_default_timezone_set('Asia/Jakarta');
 include "func.php";
 
-echo color("green","=================");echo color("nevy","[ AUTO CREATE ACCOUNT ]");echo color("green","================ \n");
-echo color("green","=================");echo color("nevy","[ AUTO CLAIM VOUCHERS ]");echo color("green","================ \n");
-echo color("green","====================");echo color("nevy","[ AUTO SET PIN ]");echo color("green","==================== \n");
+echo color("green","================="); echo color("nevy","[ AUTO CREATE ACCOUNT ]"); echo color("green","================")."\n";
+echo color("green","================="); echo color("nevy","[ AUTO CLAIM VOUCHERS ]"); echo color("green","================")."\n";
+echo color("green","===================="); echo color("nevy","[ AUTO SET PIN ]"); echo color("green","====================")."\n";
 echo " Created by : Vino AZR \n";
 echo " Version    : 555 \n";
 echo " Time       : ".date('[d-m-Y] [H:i:s]')." \n";
-echo color("purple","=================[ AUTO CREATE ACCOUNT ]================ \n");
+echo color("purple","=================[ AUTO CREATE ACCOUNT ]================")."\n";
 
 //function change(){
 	$nama = nama();
