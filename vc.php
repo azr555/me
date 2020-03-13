7e7848d35b8b731038a33a40704e68cfccd6762e
azr555secret <60191913+azr555secret@users.noreply.github.com>
2020-03-13 21:15:42 +0700

    Update vc.php

--- vc.php
+++ vc.php
@@ -5,9 +5,9 @@ include "func.php";
 echo color("green","================="); echo color("nevy","[ AUTO CREATE ACCOUNT ]"); echo color("green","================")."\n";
 echo color("green","================="); echo color("nevy","[ AUTO CLAIM VOUCHERS ]"); echo color("green","================")."\n";
 echo color("green","===================="); echo color("nevy","[ AUTO SET PIN ]"); echo color("green","====================")."\n";
-echo " Created by : Vino AZR \n";
-echo " Version    : 555 \n";
-echo " Time       : ".date('[d-m-Y] [H:i:s]')."\n";
+echo color("green"," Created by : Vino AZR")."\n";
+echo color("green"," Version    : 555")."\n";
+echo color("green"," Time       : ".date('[d-m-Y] [H:i:s]').)."\n";
 echo color("purple","=================[ AUTO CREATE ACCOUNT ]================")."\n";
 
 //function change(){
